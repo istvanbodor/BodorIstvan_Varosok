4adf01c8cb34b1b6899e09d451afe96137423451
Bodor István <istvanbodor19@gmail.com>
2022-11-25 09:55:19 +0100

    main activity, osztalyok, xmlek

---
+++ app/src/main/java/hu/petrik/varosok/ListActivity.java
@@ -0,0 +1,17 @@
+package hu.petrik.varosok;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listazott);
+    }
+
+
+}
---
+++ app/src/main/java/hu/petrik/varosok/MainActivity.java
@@ -0,0 +1,53 @@
+package hu.petrik.varosok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String base_url = "https://retoolapi.dev/LkbtQ7/varosok";
+    private Button listazogomb;
+    private Button felvevogomb;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listazogomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+                finish();
+            }
+        });
+
+        felvevogomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InsertActivity.class));
+                finish();
+            }
+        });
+
+
+
+
+
+    }
+
+
+    private void init() {
+
+        listazogomb = findViewById(R.id.listazogomb);
+        felvevogomb = findViewById(R.id.felvevogomb);
+
+    }
+}
